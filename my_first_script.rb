081baab3e315ad3ca5327d5caa0589081863d26c
Violet Mae Lim <violet25x@gmail.com>
2013-02-25 20:34:27 -0800

    Initial commit

---
+++ my_first_script.rb
@@ -0,0 +1,7 @@
+first_name = "Kris" 
+last_name = "Smith"
+age = "34"
+
+puts first_name + " " + last_name + "is" + " " + age
+puts first_name + " " + last_name + " " + "is" + " " + age
+puts first_name + " " + last_name + "is" + age
